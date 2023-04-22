67facfc977c18968a29a2f10b303dc8ffc55afcd
gz1z <127003239+gz1z@users.noreply.github.com>
2023-04-21 20:26:19 -0600

    Update create-certs.sh

--- linux-setup/create-certs.sh
+++ linux-setup/create-certs.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-sudo rm -r /etc/ssl/selfsigned
-sudo mkdir /etc/ssl/selfsigned
+sudo mkdir -p /etc/ssl/selfsigned
 cd /etc/ssl/selfsigned
+
+sudo rm -r keys
+sudo rm -r certs
+
+sudo mkdir keys
+sudo mkdir certs
+
 
 public_ip=$(curl -s ifconfig.me)
 
 sudo openssl req -x509 -nodes -newkey rsa:2048 \
--keyout ca.key -out ca.crt -subj "/C=MX/O=DreamTeam"
+-keyout keys/ca.key -out certs/ca.crt -subj "/C=MX/O=DreamTeam"
 
 sudo openssl req -nodes -newkey rsa:2048 \
--keyout server.key -out server.csr -subj "/C=MX/O=DreamTeam/CN=$public_ip"
+-keyout keys/server.key -out certs/server.csr -subj "/C=MX/O=DreamTeam/CN=$public_ip"
 
 sudo openssl req -nodes -newkey rsa:2048 \
--keyout client.key -out client.csr -subj "/C=MX/O=DreamTeam/CN=$public_ip"
+-keyout keys/client.key -out certs/client.csr -subj "/C=MX/O=DreamTeam/CN=$public_ip"
 
-sudo openssl x509 -req -in server.csr \
--CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
+sudo openssl x509 -req -in certs/server.csr \
+-CA certs/ca.crt -CAkey keys/ca.key -set_serial 01 -out certs/server.crt
 
-sudo openssl x509 -req -in client.csr \
--CA ca.crt -CAkey ca.key -set_serial 02 -out client.crt
+sudo openssl x509 -req -in certs/client.csr \
+-CA certs/ca.crt -CAkey keys/ca.key -set_serial 02 -out certs/client.crt
 
-sudo chmod 777 client.key
-sudo chmod 777 client.crt
-sudo chmod 777 client.crt
+sudo chmod 777 keys/client.key
+sudo chmod 777 certs/client.crt
+sudo chmod 777 certs/client.crt
