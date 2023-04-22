7c3123b38898e725636cf42d4c72ca1be28e2636
gz1z <127003239+gz1z@users.noreply.github.com>
2023-04-21 19:35:56 -0600

    Update create-certs.sh

--- linux-setup/create-certs.sh
+++ linux-setup/create-certs.sh
@@ -5,21 +5,21 @@ cd /etc/ssl/selfsigned
 
 public_ip=$(curl -s ifconfig.me)
 
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+sudo openssl req -x509 -nodes -newkey rsa:2048 \
 -keyout ca.key -out ca.crt -subj "/C=MX/O=DreamTeam"
 
-sudo openssl req -nodes -days 365 -newkey rsa:2048 \
+sudo openssl req -nodes -newkey rsa:2048 \
 -keyout server.key -out server.csr -subj "/C=MX/O=DreamTeam/CN=$public_ip"
 
-sudo openssl req -nodes -days 365 -newkey rsa:2048 \
+sudo openssl req -nodes -newkey rsa:2048 \
 -keyout client.key -out client.csr -subj "/C=MX/O=DreamTeam/CN=$public_ip"
 
-sudo openssl x509 -req -days 365 -in server.csr \
+sudo openssl x509 -req -in server.csr \
 -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
 
-sudo openssl x509 -req -days 365 -in client.csr \
+sudo openssl x509 -req5 -in client.csr \
 -CA ca.crt -CAkey ca.key -set_serial 02 -out client.crt
 
 sudo chmod 777 client.key
 sudo chmod 777 client.crt
-sudo chmod 777 ca.crt
+sudo chmod 777 client.crt
