8a9488a92623b565213f6389157e5ea00e23d04c
gz1z <127003239+gz1z@users.noreply.github.com>
2023-04-21 19:46:44 -0600

    Create create-certs2.sh

---
+++ linux-setup/create-certs2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo mkdir /etc/ssl/selfsigned
+cd /etc/ssl/selfsigned
+
+public_ip=$(curl -s ifconfig.me)
+
+sudo openssl req -x509 -nodes -newkey rsa:2048 \
+-keyout ca.pem -out ca.crt -subj "/C=MX/O=DreamTeam"
+
+sudo openssl req -nodes -newkey rsa:2048 \
+-keyout server.pem -out server.csr -subj "/C=MX/O=DreamTeam/CN=$public_ip"
+
+sudo openssl req -nodes -newkey rsa:2048 \
+-keyout client.pem -out client.csr -subj "/C=MX/O=DreamTeam/CN=$public_ip"
+
+sudo openssl x509 -req -in server.csr \
+-CA ca.crt -CAkey ca.pem -set_serial 01 -out server.crt
+
+sudo openssl x509 -req5 -in client.csr \
+-CA ca.crt -CAkey ca.pem -set_serial 02 -out client.crt
+
+sudo chmod 777 client.pem
+sudo chmod 777 client.crt
+sudo chmod 777 client.crt
